51a191062f03cf1ea42df7bb02e083eb7393487d
Oleg <mr.dvorezky@gmail.com>
2017-04-11 13:35:43 +0300

    change global

--- button.js
+++
@@ -1,9 +0,0 @@
-function but(){
-	if($('.main-mnu-hidden').css('display')=== 'none'){
-		$('.main-mnu-hidden').css('display', 'block');
-	}
-	else{
-		$('.main-mnu-hidden').css('display', 'none');
-	}
-	$('.main-mnu').css('display', 'none');
-}
